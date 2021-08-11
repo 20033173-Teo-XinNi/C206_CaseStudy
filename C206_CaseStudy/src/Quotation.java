6804379c022e18c6c48cc91ca8eb42ec040a4931
Bryan <Bryan@BryansLaptop>
2021-08-11 12:12:53 +0800

    -

--- C206_CaseStudy/src/Quotation.java
+++ C206_CaseStudy/src/Quotation.java
@@ -1,32 +1,33 @@
 
 public class Quotation {
+	private String quotationID;
 private String requestID;
-private String quotationID;
 private String renoCategory;
 private String description;
 private String designerName;
 private String date;
 private String amount;
 
-public Quotation(String requestID, String quotationID, String renoCategory, String description, String designerName,
+public Quotation(String quotationID, String requestID, String renoCategory, String description, String designerName,
 		String date, String amount) {
 	super();
-	this.requestID = requestID;
 	this.quotationID = quotationID;
+	this.requestID = requestID;
 	this.renoCategory = renoCategory;
 	this.description = description;
 	this.designerName = designerName;
 	this.date = date;
 	this.amount = amount;
 }
+public String getQuotationID() {
+	return quotationID;
+}
 
 public String getRequestID() {
 	return requestID;
 }
 
-public String getQuotationID() {
-	return quotationID;
-}
+
 
 public String getRenoCategory() {
 	return renoCategory;
